8759faeba2dddba6585d91aea209c4274c9824fc
gulshansinghAIT <gulshankumar_22224@aitpune.edu.in>
2025-05-02 22:15:30 +0530

    removed hosting errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { ToastContainer } from 'react-toastify'
@@ -12,7 +12,11 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+ReactDOM.createRoot(rootElement).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <React.StrictMode>
       <App />
